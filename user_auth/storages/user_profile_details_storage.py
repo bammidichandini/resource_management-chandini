2a60ac300d7e640db1034c4f3f5df494c4ed4a10
Chandini <chandini.bammidi123@gmail.com>
2020-07-02 04:48:47 +0000

    add tests

--- user_auth/storages/user_profile_details_storage.py
+++ user_auth/storages/user_profile_details_storage.py
@@ -1,10 +1,9 @@
-from resource_management.interactors.storages.user_profile_storage_interface \
+from user_auth.interactors.storages.user_profile_storage_interface \
         import StorageInterface
 from abc import ABC
 from abc import abstractmethod
-from resource_management.models import User
-from resource_management.dtos.dtos import UpdateProfileDto
-from resource_management.exceptions.exceptions import InvalidDetailsException
+from user_auth.models import User
+from user_auth.dtos.dtos import UpdateProfileDto
 from django.contrib.auth import *
 
 
@@ -15,13 +14,6 @@ class StorageImplementation(StorageInterface):
                             user_id: int,
                             user_profile_dto: UpdateProfileDto
                             ):
-        invalid_details = (not(type(user_profile_dto.name) == str)
-                            or not(type(user_profile_dto.email) == str)
-                            or not(type(user_profile_dto.gender) == str)
-                            or not(type(user_profile_dto.job_role) == str)
-                            or not(type(user_profile_dto.department) == str))
-        if invalid_details:
-            raise InvalidDetailsException
 
         User.objects.filter(id=user_id).update(
             name=user_profile_dto.name,
@@ -34,5 +26,5 @@ class StorageImplementation(StorageInterface):
 
     def update_password(self, user_id: int, password: str):
         user = User.objects.get(id=user_id)
-        user.make_password(password)
+        user.set_password(password)
         user.save()
