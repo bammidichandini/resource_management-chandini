b425248c0941874e25ab1adbd77a64fdd4295836
Chandini <chandini.bammidi123@gmail.com>
2020-07-21 01:50:46 +0000

    changes

--- user_authentication_app/presenters/authentication_presenter.py
+++ user_authentication_app/presenters/authentication_presenter.py
@@ -17,8 +17,18 @@ class PresenterImplementation(PresenterInterface):
 
 
     def raise_invalid_password_exception(self):
-        pass
+        from user_authentication_app.constants.exception_messages import INVALID_PASSWORD
+        import json
+        response_object = response.HttpResponse(
+            json.dumps({"response": INVALID_PASSWORD[0],
+                        "http_status_code": 403,
+                        "res_status": INVALID_PASSWORD[1]}), 403)
+        return response_object
 
 
     def get_login_response(self, access_token_obj: AccessTokenDTO, is_admin: bool):
-        pass
+        response_dict = {
+            "access_token": access_token_obj.access_token,
+            "is_admin": is_admin
+        }
+        return response_dict
